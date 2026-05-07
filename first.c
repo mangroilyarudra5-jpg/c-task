2f5bba4f7a3829838046da08f600ace4c994f44d
mangroilyarudra5-jpg <mangroilyarudra5@gmail.com>
2026-05-07 11:18:52 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,47 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int f,c;
+    printf("enter the value of c");
+    scanf("%d",&c);
+    f=(9/5*c)+32;
+    printf("f=%d",f);
+
+    return 0;
+}
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+ int bs,hra, da,ta,gs;
+ printf("enter the value of bs");
+ scanf("%d",&bs);
+ hra=0.1*bs;
+ da=0.05*bs;
+ ta=0.08*bs;
+ gs=bs+hra+da+ta;
+ printf("gs=%d",gs);
+ 
+
+    return 0;
+}
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int friang,secang,thrang;
+    printf("enter the value of fri ang,sec ang");
+    scanf("%d%d",&friang, &secang);
+    thrang=180-(friang+secang);
+    printf("thrang=%d",thrang);
+
+    return 0;
+}
